e55a29d28069b75a7925b9ed3c8acb432a1fb9a7
epicvoidz <eskaggs@my.athens.edu>
2025-04-13 16:21:51 -0500

    modified

--- AcademicPlanSoftware/CS452DBCourses.session.sql
+++
@@ -1,4 +0,0 @@
--- SQLBook: Code
-INSERT INTO courses (courseNum, courseName)
-VALUES ('CS307', 'Foundations of Web Development')
-ORDER BY FIELD(courseNum, 'CS305', 'CS307', 'CS309');
--- AcademicPlanSoftware/CS452DBmySQL.session.sql
+++
@@ -1,4 +0,0 @@
--- SQLBook: Code
-UPDATE cs_ite_catalog
-SET Fall_Odd = 'DL'
-WHERE courseNum IN ('CS307', 'ITE301', );
